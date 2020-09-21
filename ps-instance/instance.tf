432aba8bc6576483978299d3a1bf65b8ecf87d61
Dan Iverson <dan@daniverson.com>
2020-09-21 14:05:17 -0500

    Skip iscsi processing

--- ps-instance/instance.tf
+++ ps-instance/instance.tf
@@ -105,50 +105,50 @@ resource "oci_core_volume_attachment" "psinstance_storage_attachment" {
   # Not used with iscsi attachments
   # device = "/dev/oracleoci/oraclevdb"
 
-  connection {
-    type        = "ssh"
-    host        = var.enable_public_ip ? element(oci_core_instance.psinstance.*.public_ip, count.index) : element(oci_core_instance.psinstance.*.private_ip, count.index)
-    user        = "opc"
-    private_key = tls_private_key.public_private_key_pair.private_key_pem
-  }
+  # connection {
+  #   type        = "ssh"
+  #   host        = var.enable_public_ip ? element(oci_core_instance.psinstance.*.public_ip, count.index) : element(oci_core_instance.psinstance.*.private_ip, count.index)
+  #   user        = "opc"
+  #   private_key = tls_private_key.public_private_key_pair.private_key_pem
+  # }
   
-  # https://medium.com/oracledevs/managing-oracle-cloud-infrastructure-iscsi-block-volume-attachments-with-terraform-97726691b842
-  # register and connect the iSCSI block volume
-  provisioner "remote-exec" {
-    inline = [
-      "sudo iscsiadm -m node -o new -T ${self.iqn} -p ${self.ipv4}:${self.port}",
-      "sudo iscsiadm -m node -o update -T ${self.iqn} -n node.startup -v automatic",
-      "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.authmethod -v CHAP",
-      "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.username -v ${self.chap_username}",
-      "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.password -v ${self.chap_secret}",
-      "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -l",
-    ]
-  }
-  # initialize partition and file system
-  provisioner "remote-exec" {
-    inline = [
-      "set -x",
-      "export DEVICE_ID=ip-${self.ipv4}:${self.port}-iscsi-${self.iqn}-lun-1",
-      "export HAS_PARTITION=$(sudo partprobe -d -s /dev/disk/by-path/$${DEVICE_ID} | wc -l)",
-      "if [ $HAS_PARTITION -eq 0 ] ; then",
-      "  (echo o; echo n; echo p; echo 1; echo ''; echo ''; echo w) | sudo fdisk /dev/disk/by-path/$${DEVICE_ID}",
-      "  while [[ ! -e /dev/disk/by-path/$${DEVICE_ID}-part1 ]] ; do sleep 1; done",
-      "  sudo mkfs.ext4 /dev/disk/by-path/$${DEVICE_ID}-part1",
-      "fi",
-    ]
-  }
-  # mount the partition
-  provisioner "remote-exec" {
-    inline = [
-      "set -x",
-      "export DEVICE_ID=ip-${self.ipv4}:${self.port}-iscsi-${self.iqn}-lun-1",
-      "sudo mkdir -p ${var.volume_mount_directory}",
-      "export UUID=$(sudo /sbin/blkid -s UUID -o value /dev/disk/by-path/$${DEVICE_ID}-part1)",
-      # "export UUID=$(sudo blkid | grep /dev/disk/by-path/$${DEVICE_ID}-part1 | awk '{print $2}')",
-      "echo 'UUID='$${UUID}' ${var.volume_mount_directory} auto defaults,_netdev,nofail 0 2' | sudo tee -a /etc/fstab",
-      "sudo mount -a",
-    ]
-  }
+  # # https://medium.com/oracledevs/managing-oracle-cloud-infrastructure-iscsi-block-volume-attachments-with-terraform-97726691b842
+  # # register and connect the iSCSI block volume
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo iscsiadm -m node -o new -T ${self.iqn} -p ${self.ipv4}:${self.port}",
+  #     "sudo iscsiadm -m node -o update -T ${self.iqn} -n node.startup -v automatic",
+  #     "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.authmethod -v CHAP",
+  #     "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.username -v ${self.chap_username}",
+  #     "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -o update -n node.session.auth.password -v ${self.chap_secret}",
+  #     "sudo iscsiadm -m node -T ${self.iqn} -p ${self.ipv4}:${self.port} -l",
+  #   ]
+  # }
+  # # initialize partition and file system
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "set -x",
+  #     "export DEVICE_ID=ip-${self.ipv4}:${self.port}-iscsi-${self.iqn}-lun-1",
+  #     "export HAS_PARTITION=$(sudo partprobe -d -s /dev/disk/by-path/$${DEVICE_ID} | wc -l)",
+  #     "if [ $HAS_PARTITION -eq 0 ] ; then",
+  #     "  (echo o; echo n; echo p; echo 1; echo ''; echo ''; echo w) | sudo fdisk /dev/disk/by-path/$${DEVICE_ID}",
+  #     "  while [[ ! -e /dev/disk/by-path/$${DEVICE_ID}-part1 ]] ; do sleep 1; done",
+  #     "  sudo mkfs.ext4 /dev/disk/by-path/$${DEVICE_ID}-part1",
+  #     "fi",
+  #   ]
+  # }
+  # # mount the partition
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "set -x",
+  #     "export DEVICE_ID=ip-${self.ipv4}:${self.port}-iscsi-${self.iqn}-lun-1",
+  #     "sudo mkdir -p ${var.volume_mount_directory}",
+  #     "export UUID=$(sudo /sbin/blkid -s UUID -o value /dev/disk/by-path/$${DEVICE_ID}-part1)",
+  #     # "export UUID=$(sudo blkid | grep /dev/disk/by-path/$${DEVICE_ID}-part1 | awk '{print $2}')",
+  #     "echo 'UUID='$${UUID}' ${var.volume_mount_directory} auto defaults,_netdev,nofail 0 2' | sudo tee -a /etc/fstab",
+  #     "sudo mount -a",
+  #   ]
+  # }
   # unmount and disconnect on destroy
   # New Terraform versions error with destroy-time provisions that use connection information 
   #    without "self." references. This provision was referencing the instance to get the 
